e2b29c8d1641bd88ec04c2ae5e5f795d23d1dc1e
Jonatan M Gomes <jonatan.mim@hotmail.com>
2022-11-22 10:35:41 -0300

    finalizando desafio parte 2

--- algorithm.js
+++ algorithm.js
@@ -12,21 +12,33 @@ function valueChange(e){
     console.log(e.target.value)
 }
 
+function saveLocalS(e){
+    window.localStorage.setItem(e.target.name, e.target.value);
+}
+
 function preparingLog(e){
     console.log('Preparando envio do formulário...');
     event.preventDefault();
 }
 
+function reloadInfo(e){
+    shopName.value = window.localStorage.getItem('shopName');
+    shopAddress.value = window.localStorage.getItem('shopAddress');
+    openingTime.value = window.localStorage.getItem('openingTime');
+    closingTime.value = window.localStorage.getItem('closingTime');
+}
+
 shopName.addEventListener('focus', focusBorderColorChange);
-shopName.addEventListener('change', valueChange);
+shopName.addEventListener('change', saveLocalS);
 
 shopAddress.addEventListener('focus', focusBorderColorChange);
-shopAddress.addEventListener('change', valueChange);
+shopAddress.addEventListener('change', saveLocalS);
 
 openingTime.addEventListener('focus', focusBorderColorChange);
-openingTime.addEventListener('change', valueChange);
+openingTime.addEventListener('change', saveLocalS);
 
 closingTime.addEventListener('focus', focusBorderColorChange);
-closingTime.addEventListener('change', valueChange);
+closingTime.addEventListener('change', saveLocalS);
 
 shopForm.addEventListener('submit', preparingLog);
+window.addEventListener('load', reloadInfo);
